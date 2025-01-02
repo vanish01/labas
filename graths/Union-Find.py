e5a1b705fb1cb0d787741e371a982e80551f29bc
i.r.shatalov <isatalov70@gmail.com>
2025-01-02 23:59:42 +0300

    some graths

--- graths/Union-Find.py
+++ graths/Union-Find.py
@@ -1,29 +1,15 @@
 class UnionFind:
     def __init__(self, size):
-        """
-        Инициализация структуры Union-Find.
-        :param size: Количество элементов в структуре.
-        """
         self.parent = list(range(size))  # Каждый элемент — родитель сам себе
         self.rank = [0] * size  # Все элементы имеют ранг 0
 
     def find(self, x):
-        """
-        Операция Find с сжатием путей.
-        :param x: Элемент, для которого ищем корень.
-        :return: Корень множества, к которому принадлежит x.
-        """
         if self.parent[x] != x:
             # Рекурсивное обновление родителя
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
     def union(self, x, y):
-        """
-        Операция Union с учётом ранга.
-        :param x: Первый элемент.
-        :param y: Второй элемент.
-        """
         root_x = self.find(x)
         root_y = self.find(y)
 
@@ -38,19 +24,9 @@ class UnionFind:
                 self.rank[root_x] += 1
 
     def connected(self, x, y):
-        """
-        Проверяет, находятся ли элементы x и y в одном множестве.
-        :param x: Первый элемент.
-        :param y: Второй элемент.
-        :return: True, если элементы соединены.
-        """
         return self.find(x) == self.find(y)
 
 def test_union_find():
-    """
-    Тестирование структуры Union-Find.
-    """
-    # Создаём Union-Find для 10 элементов
     uf = UnionFind(10)
 
     # Объединяем множества
